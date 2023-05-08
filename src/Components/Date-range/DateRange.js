af446788629158ea72c7e8493b203d450da4bc6c
Sadaf <rahmansadaf46@gmail.com>
2023-05-08 20:47:59 +0600

    button update

--- src/Components/Date-range/DateRange.js
+++ src/Components/Date-range/DateRange.js
@@ -3,7 +3,11 @@ import { DateRangePicker } from 'react-date-range';
 import { useState } from 'react';
 import { addDays, subDays } from 'date-fns';
 const DateRange = () => {
-    const [state, setState] = useState([
+    const [selectedDate, setSelectedDate] = useState( {
+        startDate: null,
+        endDate: null
+    })
+    const [range, setRange] = useState([
         {
             startDate: subDays(new Date(), 0),
             endDate: addDays(new Date(), 0),
@@ -12,7 +16,7 @@ const DateRange = () => {
     ]);
     const handleOnChange = (ranges) => {
         const { selection } = ranges;
-        setState([selection]);
+        setRange([selection]);
     };
     const handleExtraMenu = (type) => {
         if (type === "This year") {
@@ -46,13 +50,37 @@ const DateRange = () => {
     const formattedDate = (date) => {
         return date.toLocaleDateString("en-US", { month: 'long', day: 'numeric', year: 'numeric' });
     }
+
+    const handleDateView =(type)=>{
+        if(type === 'Apply'){
+            setSelectedDate({
+                startDate: range[0].startDate,
+                endDate: range[0].endDate
+            })
+        }
+        else{
+            setSelectedDate({
+                startDate: null,
+                endDate: null
+            })
+            const now = new Date();
+            const formatDate = [{
+                selection: {
+                    startDate: now,
+                    endDate: now,
+                    key: "selection"
+                }
+            }]
+            handleOnChange(formatDate[0])
+        }
+    }
     return (
         <div className="date-range">
             <DateRangePicker
                 onChange={handleOnChange}
                 showSelectionPreview={true}
                 moveRangeOnFirstSelection={false}
-                ranges={state}
+                ranges={range}
                 direction="horizontal"
             />
             <div className='extra-menu'>
@@ -62,23 +90,24 @@ const DateRange = () => {
             </div>
             <div className="date-range-main">
                 <div className="date-range-data">
-                    <div className="rdrDateDisplay" style={{ color: 'rgb(62,207,142)' }}><span className="rdrDateInput rdrDateDisplayItem rdrDateDisplayItemActive"><input placeholder="Early"
-                        value={formattedDate(state[0].startDate)}
+                    <div className="rdrDateDisplay"><span className="rdrDateInput rdrDateDisplayItem rdrDateDisplayItemActive"><input placeholder="Start Date"
+                       readOnly value={selectedDate.startDate ? formattedDate(selectedDate.startDate):''}
                     /></span>
                         <span className='date-range-mid'>-</span>
-                        <span className="rdrDateInput rdrDateDisplayItem">
-                            <input placeholder="Continuous"
-                                value={formattedDate(state[0].endDate)}
+                        <span className="rdrDateInput rdrDateDisplayItem rdrDateDisplayItemActive">
+                            <input placeholder="End Date"
+                            readOnly
+                                value={selectedDate.startDate ? formattedDate(selectedDate.endDate):''}
                             /></span>
                     </div>
                 </div>
             </div>
             <div className='submit-section'>
-                <button className='cancel-btn'>
+                <button onClick={()=>handleDateView('Cancel')}className='cancel-btn'>
                     Cancel
                 </button>
                 &nbsp;
-                <button className='apply-btn'>
+                <button onClick={()=>handleDateView('Apply')} className='apply-btn'>
                     Apply
                 </button>
             </div>
